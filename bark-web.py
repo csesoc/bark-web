d490b582bace339db3f8b353c0bfedc1b1a89fc6
Christopher Manouvrier <cmanou@mac.com>
2013-02-23 22:11:12 +1100

    Add show event method

--- bark-web.py
+++ bark-web.py
@@ -19,7 +19,7 @@ def login():
     		'password' : request.form['password'],
 		}
 
-		response = requests.post(url,data=json.dumps(request_data),headers=request_headers)
+        response = requests.post(url,data=json.dumps(request_data),headers=request_headers)
 
         if response.text['status'] == 'REQUEST_DENIED':
 	        error = response.text['error_details']
@@ -29,23 +29,35 @@ def login():
             return redirect(url_for('events'))
     return render_template('login.html', error=error)
 
-@app.route('/events/')
-def events():
-    return 'events page'
+
 
 @app.route('/events/<int:event_id>/edit', methods=['GET', 'POST'])
-def edit_event(event):
-	return ''
+def edit_event(event_id):
+	return 'edit'
 
-@app.route('/event/<int:event_id>', methods=['GET'])
-def show_event(event):
-	return ''
+@app.route('/events/<int:event_id>', methods=['GET'])
+def show_event(event_id):
+    url =  app.config['api_url'] + 'events/' + event_id
+    request_headers = {'Content-Type': 'application/json'}
+    request_data = {
+		'auth_token' : session['auth_token'],
+	}
+
+    response = requests.post(url,data=json.dumps(request_data),headers=request_headers)
+    if response.text['status'] == 'REQUEST_DENIED':
+	    error = response.text['error']
+    else:
+        event = response
+    return render_template('events.html', event=event)
     
+@app.route('/events/')
+def events():
 
+    return 'events page'
 
 
 if __name__ == '__main__':
-	app.DEBUG = True
-	app.secret_key = 'aslkdjf;lsakdjf;alksdjf;lkj'
-	app.api_url = 'http://localhost:5000/'
+    app.debug = True
+    app.secret_key = 'aslkdjf;lsakdjf;alksdjf;lkj'
+    app.api_url = 'http://localhost:5000/'
     app.run(port=4444)
