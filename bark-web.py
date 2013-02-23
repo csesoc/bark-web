5d7a35e8bfe3cf216f7bd504c153541b59034412
Christopher Manouvrier <cmanou@mac.com>
2013-02-23 21:52:42 +1100

    Login should be working

--- bark-web.py
+++ bark-web.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+import requests, json
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -7,24 +10,42 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == 'POST':
-    	# do the login and save authkey in session cookies
-    else:
-        # show the login form
+    	url =  app.config['api_url'] + 'login'
+        request_headers = {'Content-Type': 'application/json'}
+        request_data = {
+			'username' : request.form['username'],
+    		'password' : request.form['password'],
+		}
 
-@app.route('/event/<int:event_id>/edit', methods=['GET', 'POST'])
+		response = requests.post(url,data=json.dumps(request_data),headers=request_headers)
+
+        if response.text['status'] == 'REQUEST_DENIED':
+	        error = response.text['error_details']
+        else:
+            session['auth_token'] = response.text['auth_token']
+            flash('You were logged in')
+            return redirect(url_for('events'))
+    return render_template('login.html', error=error)
+
+@app.route('/events/')
+def events():
+    return 'events page'
+
+@app.route('/events/<int:event_id>/edit', methods=['GET', 'POST'])
 def edit_event(event):
-    if request.method == 'POST':
-    	# save event info
-    else:
-        # show event edit form
+	return ''
 
 @app.route('/event/<int:event_id>', methods=['GET'])
 def show_event(event):
-	 # show the event
+	return ''
     
 
 
 
 if __name__ == '__main__':
-    app.run()
+	app.DEBUG = True
+	app.secret_key = 'aslkdjf;lsakdjf;alksdjf;lkj'
+	app.api_url = 'http://localhost:5000/'
+    app.run(port=4444)
